0027f2948b9cfc0237028e86744d18b3c8b2a35c
MhdTarhini <mohamedtarhini95@gmail.com>
2023-07-20 02:20:20 +0300

    backend - create signin.php + get data from database

---
+++ backend/signin.php
@@ -0,0 +1,24 @@
+<?php
+include('connection.php');
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+// $name='mohamad';
+
+$query = $mysqli->prepare('select id,username,password from user where username=?');
+$query->bind_param('s', $username);
+$query->execute();
+
+$query->store_result();
+$query->bind_result($id, $username, $password);
+$query->fetch();
+$num_rows = $query->num_rows();
+if ($num_rows == 0) {
+    $response['status'] = "user not found";
+} else {
+        $response['status'] = 'logged in';
+        $response['id'] = $id;
+        $response['password'] = $password;
+        $response['username'] = $username;
+}
+echo json_encode($response);
