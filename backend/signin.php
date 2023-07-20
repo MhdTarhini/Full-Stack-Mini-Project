342b954298a706e9288038294b747d179e810c71
MhdTarhini <mohamedtarhini95@gmail.com>
2023-07-20 03:34:30 +0300

    check username and password validation

--- backend/signin.php
+++ backend/signin.php
@@ -1,11 +1,14 @@
 <?php
 include('connection.php');
 
+// Allow the following HTTP headers for CORS requests, including the Content-Type header.
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+$post_data = file_get_contents("php://input");
 
+$input_data = json_decode($post_data, true);
 
-$username = $_POST['username'];
-$password = $_POST['password'];
+$username = $input_data['username'];
+$input_password = $input_data['password'];
 // $name='mohamad';
 
 $query = $mysqli->prepare('select id,username,password from user where username=?');
@@ -16,12 +19,21 @@ $query->store_result();
 $query->bind_result($id, $username, $password);
 $query->fetch();
 $num_rows = $query->num_rows();
+
+
 if ($num_rows == 0) {
     $response['status'] = "user not found";
 } else {
+if ($num_rows == 0) {
+    $response['status'] = "user not found";
+} else {
+    if ($input_password==$password) {
         $response['status'] = 'logged in';
-        $response['id'] = $id;
-        $response['password'] = $password;
         $response['username'] = $username;
+
+    } else {
+        $response['status'] = "wrong password";
+    }
+}
 }
 echo json_encode($response);
--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,14 +1,34 @@
-let username_input = document.querySelector("#inputUsername");
+// Sign in
+let signin_button = document
+  .querySelector(".sign-in")
+  .addEventListener("click", getValidation);
 
-function getData() {
+function getValidation() {
+  let username_input = document.getElementById("inputUsername");
+  let user_password = document.getElementById("inputPassword");
+  let button = document.querySelector(".sign-in");
+  let input_data = {
+    username: username_input.value,
+    password: parseInt(user_password.value),
+  };
   fetch("http://localhost/Full%20Stack%20Mini%20Project/backend/signin.php", {
+    method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
+    body: JSON.stringify(input_data),
   })
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      if (data.status === "logged in") {
+        button.style.backgroundColor = "green";
+        isSignin(data);
+      } else {
+        button.style.backgroundColor = "red";
+      }
     });
 }
-getData();
+
+function isSignin(data) {
+  document.getElementsByClassName("username").innerHtml = data.username;
+}
